b3d0d1ffb6bafca04d1dc1a002ab1e778e34d1e4
delfipolito <delfinapolito@gmail.com>
2023-06-27 21:23:49 -0300

    Updates

--- src/sections/Whitepaper.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export default function Whitepaper() {
-  useEffect(() => {
-    window.location.replace('https://mimic.fi/whitepaper.pdf')
-  }, [])
-
-  return <div />
-}
